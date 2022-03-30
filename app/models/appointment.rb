fa25aa2ee52b8f8dd15be57e575a552c89859abc
muhammad-anas-2 <bitf16a537@pucit.edu.pk>
2022-03-30 14:32:50 +0500

    email authentication

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,13 @@
 class Appointment < ApplicationRecord
+
+  before_save { self.email = email.downcase }
+
+
   validates :name, presence: true
   validates :phone_number, presence: true
   validates :time, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false }, length: { maximum: 100 },
+            format: { with: VALID_EMAIL_REGEX }
 end
