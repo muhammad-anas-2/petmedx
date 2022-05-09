fe53fa042503e1df93c90fe0ad86d2ad2e56d7c6
muhammad-anas-2 <bitf16a537@pucit.edu.pk>
2022-05-09 11:23:45 +0500

    Added pages, link and pics.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,19 @@ class PagesController < ApplicationController
   def index
 
   end
+  def surgical_operation
+
+  end
+  def  patient_department
+
+  end
+  def lab_service
+
+  end
+  def pet_grooming
+
+  end
+  def our_service
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :appointments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index', to: "pages#index"
+  get  'pages/surgical_operation'
+  get  'pages/patient_department'
+  get  'pages/lab_service'
+  get  'pages/pet_grooming'
+  get  'pages/our_service'
   get  'vets/our_vets'
   # post  'appointments/appointment_form'
   # resources 'appointments'
